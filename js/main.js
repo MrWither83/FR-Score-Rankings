ba33cd5a4ff9a06c5ca3b9514c0a1401ac577868
MrWither83 <maxime.barniaudy@gmail.com>
2020-03-04 18:00:22 +0100

    Add base files

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTFk1cMN1qqi08Rm6Gj--Z2ygEV0uuVCp0GPCHZGDgGQrz-NtgIpzw-CviDpCNBc-I4KZwlfhspgkK8/pub?output=csv";
+let data;
+
+function loadData() {
+    Papa.parse(dataUrl, {
+        download: true,
+        complete: function(results) {
+            data = results;
+        }
+    });
+}
+
+loadData()
+console.log(data);
