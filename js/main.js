1e6409468608609dc57c8b361a657450f668a40a
MrWither83 <maxime.barniaudy@gmail.com>
2020-03-05 14:28:07 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
-const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTFk1cMN1qqi08Rm6Gj--Z2ygEV0uuVCp0GPCHZGDgGQrz-NtgIpzw-CviDpCNBc-I4KZwlfhspgkK8/pub?output=csv";
-let data;
+const rankingDataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTFk1cMN1qqi08Rm6Gj--Z2ygEV0uuVCp0GPCHZGDgGQrz-NtgIpzw-CviDpCNBc-I4KZwlfhspgkK8/pub?output=csv";
+let rankingData;
 
 function loadData() {
-    Papa.parse(dataUrl, {
+    Papa.parse(rankingDataUrl, {
         download: true,
         header: true,
         complete: function(results) {
-            data = results;
+            rankingData = results;
         }
     });
 }
 
-loadData()
-console.log(data);
+function getPlayerData(name) {
+    let playerData = [];
+    rankingData.data.forEach(player => {
+        if (player["Name"] == name) {
+            playerData.push(player);
+        }
+    });
+    return (playerData);
+}
+
+loadData();
